70d196c5c545d41012847169d87a38ea334e7982
Justin Schultz <SchultzJustin@ymail.com>
2024-07-02 22:25:17 +0000

    Add first two pedac problems for py110

---
+++ lesson_1/pedac_problem_1.py
@@ -0,0 +1,48 @@
+# def calculate_leftover_blocks(n):
+#     current_layer = 0
+#     remaining_blocks = n
+
+#     required_blocks = (current_layer + 1) ** 2
+
+#     while remaining_blocks >= required_blocks:
+#         remaining_blocks -= required_blocks
+#         current_layer += 1
+#         required_blocks = (current_layer + 1) ** 2
+
+#     return remaining_blocks
+
+
+# print(calculate_leftover_blocks(0) == 0)  # True
+# print(calculate_leftover_blocks(1) == 0)  # True
+# print(calculate_leftover_blocks(2) == 1)  # True
+# print(calculate_leftover_blocks(4) == 3)  # True
+# print(calculate_leftover_blocks(5) == 0)  # True
+# print(calculate_leftover_blocks(6) == 1)  # True
+# print(calculate_leftover_blocks(14) == 0) # True
+
+
+# My solution
+# Variables and numbers are likely unclear
+# No "required_blocks" variable is messy
+# makes numbers and calculations require an understanding of the code
+# as opposed to using variable names to deduce what's happening
+def calculate_leftover_blocks(remaining_blocks):
+    current_layer = 0
+
+    while remaining_blocks > 0:
+        current_layer += 1
+        remaining_blocks -= current_layer**2
+
+    if remaining_blocks < 0:
+        remaining_blocks += current_layer**2
+
+    return remaining_blocks
+
+
+print(calculate_leftover_blocks(0) == 0)  # True
+print(calculate_leftover_blocks(1) == 0)  # True
+print(calculate_leftover_blocks(2) == 1)  # True
+print(calculate_leftover_blocks(4) == 3)  # True
+print(calculate_leftover_blocks(5) == 0)  # True
+print(calculate_leftover_blocks(6) == 1)  # True
+print(calculate_leftover_blocks(14) == 0) # True
---
+++ lesson_1/pedac_problem_2.py
@@ -0,0 +1,116 @@
+'''
+Commented out at the bottom is my first attempt at solving the problem.
+Unfortunately, I wasn't sure how to make it work using a dictionary. I asked
+ChatGPT for a hint and it suggested using tuples instead of a dictionary, and
+using sorted() with a custom key function. This led to my solution below. Good
+practice with custom keys, since I haven't actually implemented them before.
+Additionally, the use of -tuples[1] in the custom key function to get a
+descending order was new.
+'''
+
+def sort_by_consonant_count(string_list):
+    tuple_list = []
+    new_string_list = []
+
+    def adjacent_consonant_count(string):
+        adjacent_counter = 0
+
+        string = string.replace(" ", "")
+        adjacent_string = ""
+
+        for letter in string:
+            if letter not in "aeiou":
+                adjacent_string += letter
+                if len(adjacent_string) > adjacent_counter:
+                    adjacent_counter = len(adjacent_string)
+            elif letter in "aeiou":
+                if len(adjacent_string) > adjacent_counter:
+                    adjacent_counter = len(adjacent_string)
+                    adjacent_string = ""
+
+                else:
+                    adjacent_string = ""
+
+        if adjacent_counter == 1:
+            adjacent_counter -= 1
+            return adjacent_counter
+
+        return adjacent_counter
+
+
+    for s in string_list:
+        count = adjacent_consonant_count(s)
+        tuple_list.append((s, count))
+
+
+    def get_tuple_object(tuples):
+        return -tuples[1]
+
+    sorted_tuple_list = sorted(tuple_list, key=get_tuple_object)
+
+
+    for i in range(len(sorted_tuple_list)):
+        new_string_list.append(sorted_tuple_list[i][0])
+
+    return new_string_list
+
+
+my_list = ['aa', 'baa', 'ccaa', 'dddaa']
+print(sort_by_consonant_count(my_list) == ['dddaa', 'ccaa', 'aa', 'baa'])
+
+my_list = ['can can', 'toucan', 'batman', 'salt pan']
+print(sort_by_consonant_count(my_list) == ['salt pan', 'can can', 'batman', 'toucan'])
+
+my_list = ['bar', 'car', 'far', 'jar']
+print(sort_by_consonant_count(my_list) == ['bar', 'car', 'far', 'jar'])
+
+my_list = ['day', 'week', 'month', 'year']
+print(sort_by_consonant_count(my_list) == ['month', 'day', 'week', 'year'])
+
+my_list = ['xxxa', 'xxxx', 'xxxb']
+print(sort_by_consonant_count(my_list) == ['xxxx', 'xxxb', 'xxxa'])
+
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# def sort_by_consonant_count(string_list):
+#     adjacent_consonant_dict = {
+#     }
+
+#     new_string_list = []
+
+#     def adjacent_consonant_count(string):
+#         adjacent_counter = 0
+
+#         string = string.replace(" ", "")
+#         adjacent_string = ""
+
+#         for letter in string:
+#             if letter not in "aeiou":
+#                 adjacent_string += letter
+#                 if len(adjacent_string) > adjacent_counter:
+#                     adjacent_counter = len(adjacent_string)
+#             elif letter in "aeiou":
+#                 if len(adjacent_string) > adjacent_counter:
+#                     adjacent_counter = len(adjacent_string)
+#                     adjacent_string = ""
+
+#                 else:
+#                     adjacent_string = ""
+
+#         if adjacent_counter == 1:
+#             adjacent_counter -= 1
+#             return adjacent_counter
+
+#         return adjacent_counter
+
+
+#     for i in range(len(string_list)):
+#         adjacent_consonant_dict[string_list[i]] = adjacent_consonant_count(string_list[i])
+
+#     print(adjacent_consonant_dict)
+
+#     new_string_list = list(adjacent_consonant_dict)
+
+#     return new_string_list
