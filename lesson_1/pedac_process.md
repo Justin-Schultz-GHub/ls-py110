7ab07196e8546b7d310a69f12a356c7bf4ad37f4
Justin Schultz <SchultzJustin@ymail.com>
2024-07-09 20:38:10 +0000

    Completed more small problems, pedac_process.md expanded appropriately.

--- lesson_1/pedac_process.md
+++ lesson_1/pedac_process.md
@@ -98,3 +98,160 @@ Programmatic Algorithm:
     - "reverse string" = a string that is a reverse sliced copy of "new string"
 
 2. return the value of "new string" == "reverse string"
+
+
+### Running Totals
+Write a function that takes a list of numbers and returns a list with the same
+number of elements, but with each element's value being the running total from
+the original list.
+
+- Input: A list of numbers
+- Output: A new list with each element being the running total of the original list
+
+Rules:
+- Explicit
+    - Must be in a function
+    - Function takes one argument
+- Implicit
+    - Empty lists should return an empty list
+
+Questions:
+- None
+
+Data Structures:
+- List to hold new elements that will be the running count
+- A variable to hold the count of the current iteration
+
+Programmatic Algorithm:
+1. Start:
+    - define function "running total"
+    - "running total list" = empty list
+    - "running count" = 0
+
+2. For each element in the parameter, sum the total to that index and set
+    "running count" to total
+3. Append "running count" to "running total list"
+4. Return "running total list"
+
+
+### Letter Counter (Part 1)
+Write a function that takes a string consisting of zero or more space-separated
+words and returns a dictionary that shows the number of words of different sizes.
+
+Words consist of any sequence of non-space characters.
+
+- Input: A string
+- Output: A dictionary of key:value pairs with the length as the key and the
+    value as the number of words of that length (including punctuation)
+
+Rules:
+- Explicit
+    - Spaces don't count
+    - Other punctuation counts as part of a word's length (apostrophes, commas, etc.)
+- Implicit
+    - Empty strings should return an empty dictionary
+
+Questions:
+- None
+
+Data Structures:
+- A list of words from the string argument after they've been separated
+- A dictionary to hold the key value pairs
+
+Programmatic Algorithm:
+1. Start
+    - define function "word sizes"
+    - "word list" = list of words from parameter, separated by spaces
+    - "word size dictionary" = {}
+2. For word in "word list", "word size dictionary"[len(word)] += 1
+3. Return "word size dictionary"
+
+
+### Letter Counter (Part 2)
+Modify the word_sizes function from the previous exercise to exclude non-letters
+when determining word size. For instance, the word size of "it's" is 3, not 4.
+
+- Input: A string
+- Output: A dictionary of key:value pairs with the length as the key and the
+    value as the number of words of that length (excluding punctuation)
+
+Rules:
+- Explicit
+    - Punctuation doesn't count
+- Implicit
+    - Empty strings should return an empty dictionary
+    - Punctuation nor special characters used as letters count as length,
+        but they don't break the word either (e.g. "w@ll" has a length of 3)
+
+Questions:
+- None so far
+
+Data Structures:
+- List to break the string apart and remove unnecessary characters
+- A variable to hold the length of the current word
+- A dictionary to hold the key:value pairs
+- A variable to hold the new string
+
+Programmatic Algorithm:
+1. Start
+    - define function "word sizes"
+    - "word list" = list of words from parameter, separated by spaces
+    - "word size dictionary" = {}
+    - "temp string" = ""
+2. For word in "word list", if character in word is a letter, add it to temp string
+3. Append "temp string" to "word list"
+4. For word in "word list", "word size dictionary"[len(word)] += 1
+5. Return "word size dictionary"
+
+
+### Letter Swap
+Given a string of words separated by spaces, write a function that swaps the
+first and last letters of every word.
+
+You may assume that every word contains at least one letter, and that the string
+will always contain at least one word. You may also assume that each string
+contains nothing but words and spaces, and that there are no leading, trailing,
+or repeated spaces.
+
+- Input: A string of words
+- Output: A string of words with the first and last letter of each word reversed
+
+Rules:
+- Explicit:
+    - Swap first and last letter of each word
+    - Each string has no leading, trailing, or repeated spaces
+    - All strings have a length of at least 1
+- Implicit:
+    - Strings cannot be empty
+
+Questions:
+- None
+
+Data Structures:
+- A list to split the string for iterating
+- A list for holding swapped strings
+- A temporary string for holding the reversed string before appending it to the
+    swapped list
+
+Programmatic Algorithm:
+1. Start
+    - define function "swap"
+    - "word list" = list of words from the parameter string
+    - "swapped list" = empty list
+    - temp_string = empty string
+2. For word in "word list", add last character to "temp string"
+    - - "temp string += word[-1]"
+3. Add all characters after the first character to "temp string"
+    - Use the range function to iterate over the word, for the length of the word
+        minus one (since the last character is added already)
+    - Ignore characters on iteration 0 (word[0] is added after)
+4. Add first character to "temp string"
+    - "temp string += word[0]"
+5. Append temp string to "swapped list"
+6. Join "swapped list"
+
+Notes:
+- For some reason, I mixed up the syntax to slice the string (used a comma
+    instead of a colon), and this caused me to resort to the range function to
+    iterate over the word, instead of just concatenating with slicing. It works,
+    but it is quite the amalgamation...
--- lesson_1/py110_py119_small_problems.py
+++ lesson_1/py110_py119_small_problems.py
@@ -46,22 +46,153 @@
 
 # print(is_palindrome() == True)
 
-# Palindromic Strings (Part 2)
-# https://launchschool.com/exercises/4f333858?track=python
-def is_real_palindrome(string):
-    new_string = ''.join([character.lower() for character in string if character.isalpha() == True or character.isdigit() == True])
-    reverse_string = new_string[::-1]
+# # Palindromic Strings (Part 2)
+# # https://launchschool.com/exercises/4f333858?track=python
+# def is_real_palindrome(string):
+#     new_string = ''.join([character.lower() for character in string if
+#         character.isalpha() is True or character.isdigit() is True])
 
-    return new_string == reverse_string
+#     reverse_string = new_string[::-1]
 
-print(is_real_palindrome('madam') == True)           # True
-print(is_real_palindrome('356653') == True)          # True
-print(is_real_palindrome('356635') == False)         # True
-print(is_real_palindrome('356a653') == True)         # True
-print(is_real_palindrome('123ab321') == False)       # True
+#     return new_string == reverse_string
 
-# # # case doesn't matter
-print(is_real_palindrome('Madam') == True)           # True
+# print(is_real_palindrome('madam') is True)           # True
+# print(is_real_palindrome('356653') is True)          # True
+# print(is_real_palindrome('356635') is False)         # True
+# print(is_real_palindrome('356a653') is True)         # True
+# print(is_real_palindrome('123ab321') is False)       # True
 
-# # only alphanumerics matter
-print(is_real_palindrome("Madam, I'm Adam") == True) # True
+# # # # case doesn't matter
+# print(is_real_palindrome('Madam') is True)           # True
+
+# # # only alphanumerics matter
+# print(is_real_palindrome("Madam, I'm Adam") is True) # True
+
+# # Running Totals
+# # https://launchschool.com/exercises/8e9d667a?track=python
+# def running_total(numbers):
+#     running_total_list = []
+#     running_count = 0
+
+#     for i in range(len(numbers)):
+#         running_count += numbers[i]
+#         running_total_list.append(running_count)
+
+#     return running_total_list
+
+# print(running_total([2, 5, 13]) == [2, 7, 20])    # True
+# print(running_total([14, 11, 7, 15, 20])
+#       == [14, 25, 32, 47, 67])                    # True
+# print(running_total([3]) == [3])                  # True
+# print(running_total([]) == [])                    # True
+
+# # Letter Counter (Part 1)
+# # https://launchschool.com/exercises/094a11fe
+# def word_sizes(words):
+#     word_size_dictionary = {}
+
+#     if len(words) > 0:
+#         word_list = words.split(" ")
+
+#         for word in word_list:
+#             word_length = int(len(word))
+#             if word_length in word_size_dictionary:
+#                 word_size_dictionary[word_length] += 1
+#             else:
+#                 word_size_dictionary[word_length] = 1
+
+#     return word_size_dictionary
+
+# # All of these examples should print True
+
+# string = 'Four score and seven.'
+# print(word_sizes(string) == {4: 1, 5: 1, 3: 1, 6: 1})
+
+# string = 'Hey diddle diddle, the cat and the fiddle!'
+# print(word_sizes(string) == {3: 5, 6: 1, 7: 2})
+
+# string = 'Humpty Dumpty sat on a wall'
+# print(word_sizes(string) == {6: 2, 3: 1, 2: 1, 1: 1, 4: 1})
+
+# string = "What's up doc?"
+# print(word_sizes(string) == {6: 1, 2: 1, 4: 1})
+
+# print(word_sizes('') == {})
+
+# # Letter Counter (Part 2)
+# # https://launchschool.com/exercises/0871b97b
+# def word_sizes(words):
+#     word_size_dictionary = {}
+#     clean_word_list = []
+#     temp_string = ""
+
+#     if len(words) > 0:
+#         word_list = words.split()
+
+#         for word in word_list:
+#             for character in word:
+#                 if character.isalpha():
+#                     temp_string += character
+
+#             clean_word_list.append(temp_string)
+#             temp_string = ""
+
+#         for word in clean_word_list:
+#             word_length = int(len(word))
+#             if word_length in word_size_dictionary:
+#                 word_size_dictionary[word_length] += 1
+#             else:
+#                 word_size_dictionary[word_length] = 1
+
+#     return word_size_dictionary
+
+# # All of these examples should print True
+
+# string = 'Four score and seven.'
+# print(word_sizes(string) == {4: 1, 5: 2, 3: 1})
+
+# string = 'Hey diddle diddle, the cat and the fiddle!'
+# print(word_sizes(string) == {3: 5, 6: 3})
+
+# string = 'Humpty Dumpty sat on a w@ll'
+# print(word_sizes(string) == {6: 2, 3: 2, 2: 1, 1: 1})
+
+# string = "What's up doc?"
+# print(word_sizes(string) == {5: 1, 2: 1, 3: 1})
+
+# print(word_sizes('') == {})
+
+# Letter Swap
+# https://launchschool.com/exercises/81a9b8ef
+def swap(string):
+    word_list = string.split()
+    swapped_list = []
+    temp_string = ""
+
+    for word in word_list:
+        if len(word) > 2:
+            temp_string += word[-1]
+            for i in range(len(word) - 1):
+                if i > 0:
+                    temp_string += word[i]
+
+            temp_string += word[0]
+            swapped_list.append(temp_string)
+            temp_string = ""
+
+        elif len(word) == 1:
+            swapped_list.append(word)
+
+        else:
+            temp_string += word[-1]
+            temp_string += word[0]
+            swapped_list.append(temp_string)
+            temp_string = ""
+
+    new_string = ' '.join(swapped_list)
+    return new_string
+
+print(swap('Oh what a wonderful day it is')
+      == "hO thaw a londerfuw yad ti si")  # True
+print(swap('Abcde') == "ebcdA")            # True
+print(swap('a') == "a")                    # True
